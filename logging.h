85f50c730eefb2957f42d5fe1c190375d6a0d0e1
Oswald Weber <oswald_weber@hotmail.de>
2013-06-10 00:37:47 +0200

    Update 0.36: Added Database creat & Data insert Function to logging.h

--- logging.h
+++ logging.h
@@ -4,14 +4,28 @@
 using namespace std;
 
 //Erstellung der Datenbank Datei
-#define DATABSE_FILE "sqlite3_db.sql"
+#define DATABSE_FILE "Jarvis_db.sql"
 
 //Tabellen erstellen
-void CreateTable(sqlite3 *ptr, string sTableName)
+void CreateTable(sqlite3 *sq3DataBase, string sTableName)
 {
-	string buffer;
-	buffer = "CREATE TABLE " + sTableName + " (id integer primary key, name text, vorname text)";
+	string sMessageSQL;
+	sMessageSQL = "CREATE TABLE " + sTableName + " (id integer primary key, Date text, SenderUser text, SenderNick text, ReceiverUser text, ReceiverNick text, Command text, Message text)";
+	sqlite3_exec(sq3DataBase, sMessageSQL.c_str(), NULL, NULL, NULL);
+}
 
-
-	sqlite3_exec(ptr, buffer.c_str(), NULL, NULL, NULL);
+//Eintrag in die Tabelle einfügen
+void InsertData(sqlite3 *sq3DataBase, 
+		string sTableName, 
+		string sDate, 
+		string sSenderUser, 
+		string sSenderNick, 
+		string sReceiverUser, 
+		string sReceiverNick, 
+		string sCommand, 
+		string sMessage)
+{
+	string sMessageSQL;
+	sMessageSQL = "INSERT INTO " + sTableName + " (Date, SenderUser, SenderNick, ReceiverUser, ReceiverNick, Command, Message) values ('"+sDate+"' ,'"+sSenderUser+"' ,'"+sSenderNick+"' ,'"+sReceiverUser+"' ,'"+sReceiverNick+"' ,'"+sCommand+"' ,'"+sMessage+"');";
+	sqlite3_exec(sq3DataBase, sMessageSQL.c_str(), NULL, NULL, NULL);
 }
